7835c64c8511fbfe5251a782eb31e173b0191eba
carbon-ai-1 <carbonaibot+1@myrgorod.net>
2025-03-28 20:50:54 +0000

    Carbon AI: Add personalized greeting route

--- src/routes.py
+++ src/routes.py
@@ -5,3 +5,8 @@ routes = Blueprint('routes', __name__)
 def hello_world():
     current_app.logger.debug('Route "/" has been accessed.')
     return '<h1>Hello, World!</h1><img src="/static/img/flask.png">'
+
+@routes.route("/hello/<name>")
+def hello_name(name):
+    current_app.logger.debug(f'Route "/hello/{{name}}" has been accessed with name: {name}.')
+    return f'<h1>Hello, {name}!</h1>'
